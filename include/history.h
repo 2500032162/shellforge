fd5e4ce5dcb96a683712f74cba36106c3da178a7
Keerthi <2500032162@kluniversity.in>
2026-08-26 17:19:43 +0000

    Milestone 1 : Initial shellforge framework

---
+++ include/history.h
@@ -0,0 +1,10 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+
+#endif
---
+++ src/history.c
@@ -0,0 +1,5 @@
+#include "../include/history.h"
+
+// History functionality using GNU readline
+// The readline library automatically handles command history
+// add_history() is called in main.c to save each command
